d7fc680457a5640b2b2c4008b86646d6a21eaac4
thanhpv1996 <subin96nd@gmail.com>
2020-12-01 10:40:41 +0700

    baitap_buoi6

---
+++ script/script.js
@@ -0,0 +1,14 @@
+let a = prompt("Nhap vao so a");
+switch (a) {
+    case '1':
+        window.open("https://quantrimang.com")
+        break;
+    case '2':
+        window.open("https://download.com.vn")
+        break;
+    case '3':
+        window.open("https://vndoc.com")
+        break;
+    default:
+        window.open(" https://meta.vn");
+}
